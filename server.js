21793051c2f5c554954e9b30fbb383c9ed4067fe
Sparkle Party <lexi.slovik@gmail.com>
2022-01-20 11:16:53 -0800

    get path to index.html and notes.html working

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const path = require('path');
 const fs = require('fs');
 
 const app = express();
 const PORT = 3001;
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
